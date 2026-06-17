87de92563176bc937dd7e5c0c1661e8aa59b128d
arjun jaiswal <arjunjaiswal4242@gmail.com>
2026-06-17 19:53:49 +0530

    added redis url

--- packages/queue/src/redis.ts
+++ packages/queue/src/redis.ts
@@ -1,7 +1,9 @@
 import IORedis from 'ioredis';
 
-export const redisConnection = new IORedis({
-  host: process.env.REDIS_HOST || 'localhost',
-  port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
-  maxRetriesPerRequest: null,
-});
+export const redisConnection = process.env.REDIS_URL
+  ? new IORedis(process.env.REDIS_URL, { maxRetriesPerRequest: null })
+  : new IORedis({
+      host: process.env.REDIS_HOST || 'localhost',
+      port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
+      maxRetriesPerRequest: null,
+    });
